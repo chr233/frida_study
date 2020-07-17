135ebee3e2af2674df00c10e0d4cb3b4bf17e969
Chr_ <chr@chrxw.com>
2020-07-17 23:29:49 +0800

    init

--- Hook.js
+++ Hook.js
@@ -2,7 +2,7 @@
  * @Author       : Chr_
  * @Date         : 2020-02-16 18:42:39
  * @LastEditors  : Chr_
- * @LastEditTime : 2020-07-17 22:41:34
+ * @LastEditTime : 2020-07-17 23:29:41
  * @Description  : 注入代码
  */
 console.log("脚本载入成功");
@@ -21,18 +21,8 @@ Java.perform(function() {
         console.log('encrypt-in', arg1, arg2);
         //调用原函数
         var result = this.encrypt(arg1, arg2);
-
         //打印出参
         console.log('encrypt-out', result);
-
-        //解密算法
-        var skey = new SecretKeySpec(arg1, "AES");
-        var cipher = new Cipher.getInstance("AES/ECB/PKCS5PADDING");
-        cipher.init(Cipher.DECRYPT_MODE, skey);
-        var plain = String(cipher.doFinal(result));
-
-        //打印出参
-        console.log('decrypt-out', plain);
         //返回给原函数的调用
         return result;
     }
