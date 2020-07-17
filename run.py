f77b5249ef611180d5bddf06d52129dbf745ed40
Chr_ <chr@chrxw.com>
2020-07-17 20:41:04 +0800

    init

--- run.py
+++ run.py
@@ -2,8 +2,8 @@
 # @Author       : Chr_
 # @Date         : 2020-02-16 18:42:42
 # @LastEditors  : Chr_
-# @LastEditTime : 2020-07-17 20:06:39
-# @Description  : 
+# @LastEditTime : 2020-07-17 20:40:57
+# @Description  : 加载器
 '''
 
 import sys
@@ -11,7 +11,7 @@ import frida
 from typing import BinaryIO
 
 
-process_name = 'com.max.xiaoheihe'
+process_name = 'infosecadventures.fridademo'
 
 
 # 发送信息回调函数
@@ -20,6 +20,7 @@ def on_message(message, data):
         print(f"[*] {message['payload']}")
     else:
         print(message)
+
 
 if __name__ == '__main__':
     try:
@@ -30,10 +31,11 @@ if __name__ == '__main__':
         print("* 连接到Frida Server失败")
     else:
         process = device.attach(process_name)
-        #加载JS脚本
-        js = open('/Hook.js', encoding='utf-8').read()
+        # 加载JS脚本
+        js = open('hook.js', encoding='utf-8').read()
         script = process.create_script(js)
         script.on('message', on_message)
         script.load()
         # 读取返回输入
-        sys.stdin.read()
+        input()
+        script.unload()
